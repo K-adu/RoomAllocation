c6eaffafb4dd64108b21ec7484a6792b1d14e331
K-adu <manishbaral112@gmail.com>
2023-09-26 12:27:20 +0545

    fix:controller removed

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -1,19 +1,17 @@
 import { Module } from '@nestjs/common';
-import { BookingController } from './booking.controller';
 import { BookingService } from './booking.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BookingSchema } from './schema/booking.schema';
-import { SharedModule } from 'src/shared/shared.module';
 import { BookingRepository } from './booking.repository';
 import { TimeService } from './handlers/time.service';
 import { BookingResolver } from './booking.resolver';
+import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Booking', schema: BookingSchema }]),
     SharedModule,
   ],
-  controllers: [BookingController],
   providers: [BookingService, BookingRepository, TimeService, BookingResolver],
 })
 export class BookingModule {}
